25274aef019ada47a681469e553170956cd0cf38
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-11 19:11:17 +0300

    "add AuthorizationService class"

--- src/main/java/com/example/authorization/services/AuthorizationService.java
+++ src/main/java/com/example/authorization/services/AuthorizationService.java
@@ -1,4 +1,24 @@
 package com.example.authorization.services;
 
 public class AuthorizationService {
+    UserRepository userRepository;
+
+    List<Authorities> getAuthorities(String user, String password) {
+        if (isEmpty(user) || isEmpty(password)) {
+            throw new InvalidCredentials("User name or password is empty");
+        }
+        List<Authorities> userAuthorities = userRepository.getUserAuthorities(user, password);
+        if (isEmpty(userAuthorities)) {
+            throw new UnauthorizedUser("Unknown user " + user);
+        }
+        return userAuthorities;
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    private boolean isEmpty(List<?> str) {
+        return str == null || str.isEmpty();
+    }
 }
