13d6df6ab0422ea11984d8289fb9b38ed6997e64
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-11 19:10:38 +0300

    "add AuthorizationController class"

---
+++ src/main/java/com/example/authorization/controllers/AuthorizationController.java
@@ -0,0 +1,16 @@
+package com.example.authorization.controllers;
+
+import com.example.authorization.services.AuthorizationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorizationController {
+    AuthorizationService service;
+
+    @GetMapping("/authorize")
+    public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
+        return service.getAuthorities(user, password);
+    }
+}
---
+++ src/main/java/com/example/authorization/services/AuthorizationService.java
@@ -0,0 +1,4 @@
+package com.example.authorization.services;
+
+public class AuthorizationService {
+}
